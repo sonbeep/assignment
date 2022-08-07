7c8e3b409c84df60a9921584ad60d3a53430a8e8
adminn <sonnmph13824@fpt.edu.vn>
2022-08-07 16:56:17 +0700

    đ

--- routes/index.js
+++ routes/index.js
@@ -74,7 +74,7 @@ router.post('/create', async function(req, res){
   res.redirect('/')
 })
 router.get('/getUser', function (req, res){
-  const thuVienList =mongoose.model('anh', THUVIEN);
+  const thuVienList = mongoose.model('anh', THUVIEN);
 
   thuVienList.find({},function (error, result){
     res.send(result);
